ef63948934cd1d532e50f08610b1b85ee76c5f1e
Arshveer <arshveer.nagra@mail.utoronto.ca>
2026-02-02 23:30:11 -0500

    files

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"OrderService"},{"l":"ProductService"},{"l":"UserService"}];updateSearchResults();
--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"OrderService","l":"OrderServer"},{"p":"ProductService","l":"ProductServer"},{"p":"UserService","l":"UserServer"}];updateSearchResults();
